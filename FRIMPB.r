3d245753794506e79e6fad05959302f4f92b4abf
Barry Cooke <barryjamescooke1967@gmail.com>
2024-06-25 16:35:23 -0400

    Separate Jasper from AB/FRI r-values
    Make Jasper NP its own independent side-project

--- FRIMPB.r
+++ FRIMPB.r
@@ -1451,21 +1451,3 @@ lines(residuals.sum.correlog[,1],residuals.sum.correlog.mean)
 points(residuals.sum.correlog[,1],residuals.sum.correlog.mean,pch=21)
 abline(h=0)
 
-###################
-#   Jasper data   #
-#  June 18 2024   #
-###################
-
-infile<-"data/Brett/JasperData/AreaInfested20132021.txt"
-Jasper.AreaInfested<-read.table(infile,header=T)
-win.graph(height=5,width=5)
-plot(Jasper.AreaInfested$year, Jasper.AreaInfested$area,type="l",xlab="year",ylab="area infested (ha)",main="Jasper National Park")
-points(Jasper.AreaInfested$year, Jasper.AreaInfested$area,pch=19)
-
-#plot interannual change in area infested, R
-Jasper.R<-Jasper.AreaInfested$area[2:length(Jasper.AreaInfested$area)]/Jasper.AreaInfested$area[1:(length(Jasper.AreaInfested$area)-1)]
-win.graph(height=5,width=5)
-hist(Jasper.R)
-
-win.graph(height=6,width=6)
-plot(2014:2021,log10(Jasper.R),type="l",xlab="year",ylab="log10 (Area t/Area t-1)",main="Jasper National Park")
---
+++ Jasper.R
@@ -0,0 +1,25 @@
+####################################################
+# Exploratory analysis of Jasper NP r values data  #
+# published as Cooke et al 2024                    #
+# Barry J. Cooke                                   #
+# June 18, 2024                                    #
+# last change: June 25, 2024                       #
+####################################################
+
+##########################
+#   area infested data   #
+##########################
+
+infile<-"data/Brett/JasperData/AreaInfested20132021.txt"
+Jasper.AreaInfested<-read.table(infile,header=T)
+win.graph(height=5,width=5)
+plot(Jasper.AreaInfested$year, Jasper.AreaInfested$area,type="l",xlab="year",ylab="area infested (ha)",main="Jasper National Park")
+points(Jasper.AreaInfested$year, Jasper.AreaInfested$area,pch=19)
+
+#plot interannual change in area infested, R
+Jasper.R<-Jasper.AreaInfested$area[2:length(Jasper.AreaInfested$area)]/Jasper.AreaInfested$area[1:(length(Jasper.AreaInfested$area)-1)]
+win.graph(height=5,width=5)
+hist(Jasper.R)
+
+win.graph(height=6,width=6)
+plot(2014:2021,log10(Jasper.R),type="l",xlab="year",ylab="log10 (Area t/Area t-1)",main="Jasper National Park")
