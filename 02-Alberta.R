97fb5c37b3b09e59f28005a6f00970d104eda44e
Alex Chubaty <achubaty@for-cast.ca>
2024-06-27 14:29:57 -0600

    [WIP] placeholder script for Alberta analyses

---
+++ 02-Alberta.R
@@ -0,0 +1,10 @@
+# packages ------------------------------------------------------------------------------------
+
+library(DBI)
+library(odbc)
+library(ggplot2)
+library(ggspatial)
+
+# read in data from MS Access databoses -------------------------------------------------------
+
+## TODO
