64a0ad821ed7a8ceb4a3f76787dea90aeb3e12cc
Alex Chubaty <achubaty@for-cast.ca>
2024-07-02 08:38:18 -0600

    move DEM creation to dataprep script

--- 01-data-prep.R
+++ 01-data-prep.R
@@ -65,6 +65,39 @@ can1 <- st_transform(can1.latlon, targetCRS)
 ab.latlon <- can1.latlon[can1.latlon$NAME_1 == "Alberta", ]
 ab <- can1[can1$NAME_1 == "Alberta", ]
 
+# create DEM for use with BioSIM --------------------------------------------------------------
+
+## follows approach taken in LandR_MPB_studyArea and mpbClimateData modules
+absk <- geodata::gadm(country = "CAN", level = 1, path = dataPath) |>
+  sf::st_as_sf() |>
+  subset(x = _, NAME_1 %in% c("Alberta", "Saskatchewan")) |>
+  sf::st_transform(targetCRS)
+
+studyAreaReporting <- mpbutils::mpbStudyArea(
+  ecoregions = c(112, 120, 122, 124, 126),
+  targetCRS = targetCRS,
+  cPath = cachePath,
+  dPath = dataPath
+) |>
+  sf::st_intersection(absk) |>
+  sf::st_union()
+
+rasterToMatchReporting <- Cache(
+  LandR::prepInputsLCC,
+  year = 2005, ## TODO: use 2010?
+  studyArea = studyAreaReporting,
+  destinationPath = dataPath,
+  filename2 = NULL
+)
+
+DEM <- Cache(
+  LandR::prepInputsCanDEM,
+  rasterToMatch = rasterToMatchReporting,
+  studyArea = studyAreaReporting,
+  destinationPath = dataPath
+) |>
+  writeRaster(file.path(outputPath, "DEM_ABSK_studyArea.tif")) ## TODO: different file format?
+
 # get pine maps -------------------------------------------------------------------------------
 
 ## TODO: issue #3
--- 02-Alberta.R
+++ 02-Alberta.R
@@ -5,40 +5,6 @@ library(odbc)
 library(ggplot2)
 library(ggspatial)
 
-# create DEM for use with BioSIM --------------------------------------------------------------
-
-## follows approach taken in LandR_MPB_studyArea and mpbClimateData modules
-absk <- geodata::gadm(country = "CAN", level = 1, path = dataPath) |>
-  sf::st_as_sf() |>
-  subset(x = _, NAME_1 %in% c("Alberta", "Saskatchewan")) |>
-  sf::st_transform(targetCRS)
-
-studyAreaReporting <- mpbutils::mpbStudyArea(
-  ecoregions = c(112, 120, 122, 124, 126),
-  targetCRS = targetCRS,
-  cPath = cachePath,
-  dPath = dataPath
-) |>
-  sf::st_intersection(absk) |>
-  sf::st_union()
-
-rasterToMatchReporting <- Cache(
-  LandR::prepInputsLCC,
-  year = 2005, ## TODO: use 2010?
-  studyArea = studyAreaReporting,
-  destinationPath = dataPath,
-  filename2 = NULL
-)
-
-DEM <- Cache(
-  LandR::prepInputsCanDEM,
-  rasterToMatch = rasterToMatchReporting,
-  studyArea = studyAreaReporting,
-  destinationPath = dataPath
-) |>
-  writeRaster(file.path(outputPath, "DEM_ABSK_studyArea.tif")) ## TODO: different file format?
-
-
 # read in data from MS Access databases -------------------------------------------------------
 
 ## TODO
