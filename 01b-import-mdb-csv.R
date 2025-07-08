18895d76029e954817e7c88f4552811ee26c2684
Barry Cooke <barryjamescooke1967@gmail.com>
2025-07-08 08:50:49 -0400

    comments
    how to treat variables

--- 01b-import-mdb-csv.R
+++ 01b-import-mdb-csv.R
@@ -27,6 +27,45 @@ csv_files <- dataPath |>
   list.files(pattern = "_mpb_(site|trees)[.]csv", full.names = TRUE, recursive = TRUE) |>
   fs::path_rel()
 
+#Notes
+
+#From the trees files we only want:
+#-siteID
+#-lat_dd, lon_dd
+#-dbh
+#-ht_pitch_tube
+#-all the survival numbers (ns1 etc)
+#-r_value
+#We don't care about crew, access, or any of the district information.
+
+#From the site files we only want:
+#-siteID
+#-plot_lat_dd
+#-plot_lon_dd
+#-nbr_infest (or nbr_trees)
+#-r_value
+#We don't care about crew, access, or any of the district information.
+
+#1. Note that the names of the geospatials differ between site & tree files. (site files pre-pend _dd with "plot".)
+#   Good. When we join the tables we want to keep both sets of geospatials, site and tree.
+#   So we can use the names as they exist. The reason we are
+#   keeping both sets of spatials is in case siteIDs are erroneous, missing or don't match.
+#   We can always associate site with tree by geospatial proximity if siteID ever fails.
+#   Also, if lat/lons in one file are erroneous, they can be guessed at from the other.
+#   Why are we doing this? Because we do not want to lose a single r_value because of simple metadata snafu.
+
+#2. Note that r_value exists in both files under the same name. We will not analyze either,
+#   but we want to examine the consequences of computing r_value different ways, including their
+#   two ways, using whatever method they used. We what to know if method matters.
+#   We also want to know if scale matters. So we will want to name these pre-computed variables
+#   "r_value_site" and "r_value_tree", as distinguished from the "r_value" that we
+#   will compute from the "ns1" etc. data. In our computation,
+#   we will test what happens when we alter the denominator policy. Dividing by zero holes gives an Inf, which can't be used in a model.
+#   But zero holes happens more often than you'd like, because the disks are only 4" circles, not 12" squares. Adding 1 to every
+#   every denominator biases the r-value low ... but (a) the bias is small (and measurable), and (b) we don't care about absolute values
+#   in our model. We care about spatial variation in pattern.
+
+
 ## read in each (set of) files, validate, and merge
 all_data <- dirname(dirname(csv_files)) |>
   unique() |>
@@ -62,7 +101,7 @@ browser()
         ) |>
         mutate(
           ## type checking
-          access_lat_dmd = as.character(access_lat_dmd), ## TODO: verify format - why *lat_dmd have 2 values?
+          access_lat_dmd = as.character(access_lat_dmd), ## TODO: verify format - why *lat_dmd have 2 values? REPLY: These are degrees minutes decimal (dmd). We want decimal degrees (dd).
           access_long_dmd = as.character(access_long_dmd), ## TODO: verify format - why *lond_dmd have 2 values?
           corp_area = as.character(corp_area),
           project = as.character(project),
@@ -88,3 +127,4 @@ browser()
       purrr::list_rbind()
 }) |>
   purrr::list_rbind()
+
