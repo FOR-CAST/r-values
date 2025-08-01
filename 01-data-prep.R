eb0327ceff2dc704640a1bb5f91c462e86ff3ac0
Alex Chubaty <achubaty@for-cast.ca>
2025-08-01 14:54:32 -0600

    cleanup pkg deps
    - use recommended `renv` approach to deal with suggested package use;
    - ignore retired spatial packages;

--- 01-data-prep.R
+++ 01-data-prep.R
@@ -10,21 +10,6 @@ library(terra)
 library(reproducible)
 library(LandR)
 
-## NOTE: several suggested packages used here indirectly;
-## library() calls included here to appease renv.
-if (FALSE) {
-  library(BioSIM)
-  library(cluster)
-  library(foreign)
-  library(httpuv)
-  library(J4R)
-  library(later)
-  library(promises)
-  library(rpart)
-  library(spatial)
-  library(XML)
-}
-
 # setup ---------------------------------------------------------------------------------------
 
 ## paths
---
+++ _dependencies.R
@@ -0,0 +1,14 @@
+## NOTE: several suggested packages used here indirectly;
+## library() calls included here to appease renv.
+if (FALSE) {
+  library(BioSIM)
+  library(cluster)
+  library(foreign)
+  library(httpuv)
+  library(J4R)
+  library(later)
+  library(promises)
+  library(rpart)
+  library(spatial)
+  library(XML)
+}
